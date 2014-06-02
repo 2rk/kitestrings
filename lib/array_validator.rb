4404d8ec2c97671ad7bf88150ee484870be0e5fb
Matt Connolly <matt.connolly@tworedkites.com>
2014-06-02 13:53:18 +1000

    Adding ArrayValidator

---
+++ lib/array_validator.rb
@@ -0,0 +1,21 @@
+class ArrayValidator < ActiveModel::EachValidator
+  # Validate the value is an Array and optionally that the number of items in it is at least a minimum or at most a
+  # maximum.
+  #
+  # Example:
+  #  validates :address_states, array: {min: 1, min_message: "must have at least 1 State selected"},
+  #
+  def validate_each(record, attribute, value)
+    if value.is_a? Array
+      if options[:min].present? && value.size < options[:min]
+        record.errors.add attribute, options[:min_message] || "must have at least #{options[:min]} #{'item'.pluralize(options[:min])}"
+      end
+
+      if options[:max].present? && value.size > options[:max]
+        record.errors.add attribute, options[:max_message] || "must have at most #{options[:max]} #{'item'.pluralize(options[:max])}"
+      end
+    else
+      record.errors.add attribute, "must be an array"
+    end
+  end
+end
---
+++ spec/lib/array_validator_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+require 'active_support'
+require 'active_model'
+require 'array_validator'
+
+class ThingWithItems
+  include ActiveModel::Validations
+  attr_accessor :items
+
+  def initialize(items)
+    @items = items
+  end
+
+  validates :items, array: {min: 1, max: 5}
+end
+
+describe ArrayValidator do
+
+  before { subject.valid? }
+
+  context "validating an object with no items" do
+    subject { ThingWithItems.new(nil) }
+    it { expect(subject.errors[:items]).to include("must be an array") }
+  end
+
+  context "validating an object with an empty array" do
+    subject { ThingWithItems.new([])}
+    it { expect(subject.errors[:items]).to include("must have at least 1 item") }
+  end
+
+  context "validating an object with an empty array" do
+    subject { ThingWithItems.new([1,2,3,4,5,6])}
+    it { expect(subject.errors[:items]).to include("must have at most 5 items") }
+  end
+
+  context "validation passes" do
+    subject { ThingWithItems.new([1,2,3])}
+    it { expect(subject.errors[:items]).to be_empty }
+  end
+end
