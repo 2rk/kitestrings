b95babdfe99d43ad22cc4c21f62a72b52ba80380
Tom Ridge <tom.ridge@tworedkites.com>
2015-02-02 10:59:33 +1000

    adjust rakefile generation, version

--- lib/kitestrings/railtie.rb
+++ lib/kitestrings/railtie.rb
@@ -4,10 +4,6 @@ module Kitestrings
   class Railtie < ::Rails::Railtie
     railtie_name :kitestrings
 
-    rake_tasks do
-      load "tasks/test_prepare_after_migrate.rake"
-    end
-
     initializer 'kitestrings.autoload', :before => :set_autoload_paths do |app|
       app.config.autoload_paths << File.expand_path("../../", __FILE__)
     end
